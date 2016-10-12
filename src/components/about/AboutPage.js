417d11fa81c1d2b7eb9b9c265252751e2a1f2487
g1ng3r <G@g1ng3rs-Mac-mini.local>
2016-10-11 22:00:22 -0400

    add routing / initial components

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Adminstration</h1>
+				<p>React, Redux, and React Router in ES6 for ultraresponsive web apps.</p>
+			</div>
+		);
+	}
+}
+
+export default AboutPage;
