3a29b7b2b41da62fb77bbe130685ecce3572ff8a
matthew-elia <matt.elia@gramercytech.com>
2016-10-13 20:20:00 -0400

    mock api / wire up store->presentation

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -2,10 +2,8 @@ import * as types from '../actions/actionTypes.js';
 
 export default function courseReducer(state=[], action){
 	switch(action.type) {
-		case 'CREATE_COURSE':
-			return [...state,
-				Object.assign({}, action.course)
-			];
+		case 'LOAD_COURSES_SUCCESS':
+			return action.courses;
 
 		default:
 			return state; 
